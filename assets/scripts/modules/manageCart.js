23ee2d22b14a6a444aeee946c4adce6cd5a962a6
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2025-10-03 15:14:34 +0200

    Add shopping cart content

--- assets/scripts/modules/manageCart.js
+++ assets/scripts/modules/manageCart.js
@@ -1,3 +1,5 @@
+import { getProductData } from "./getProductData.js";
+
 document.addEventListener("DOMContentLoaded", () => {
     const cartButton = document.getElementById("cart");
     const cartModal = document.getElementById("shopping-cart-modal");
@@ -6,12 +8,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
     cartButton.addEventListener("click", () => {
         cartModal.classList.toggle("active");
+        const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
         if (cartModal.classList.contains("active")) {
-            const cart = JSON.parse(localStorage.getItem("cart")) || [];
+            // const cart = JSON.parse(localStorage.getItem("cart")) || [];
             const amountOfItems = cart.reduce((sum, item) => sum + Number(item.amount), 0);
             
             amountOfItems <= 0 ? cartModal.classList.add("empty") : cartModal.classList.remove("empty")
+        };
+
+        if (cartModal.classList.contains("active") && !cartModal.classList.contains("empty")) {
+            const productList = document.getElementById("product-list");
+            
+            cart.forEach(item => {
+                getProductData(item.id).then(data => {
+                    const productData = data;
+
+                    const newNode = document.createElement("li");
+                    Object.assign(newNode, {
+                        className: "item-wrapper"
+                    });
+
+                    const imageNode = document.createElement("img");
+                    Object.assign(imageNode, {
+                        src: productData.images[0].thumb,
+                        alt: productData.name
+                    });
+                    newNode.appendChild(imageNode);
+
+                    // Chekcs inbouwen voor als bepaalde data niet bestaat
+
+
+                    productList.appendChild(newNode);
+                    console.log(productData)
+                });
+
+                
+            });
         };
     });
 
