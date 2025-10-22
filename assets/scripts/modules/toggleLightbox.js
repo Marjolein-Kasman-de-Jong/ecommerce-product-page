0516c851f7f296cd1b4f1a99420e6a5c63fe032d
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2025-10-22 15:17:20 +0200

    Make gallery show the same image as lightbox when lightbox is closed, fix blue highlight effects

--- assets/scripts/modules/toggleLightbox.js
+++ assets/scripts/modules/toggleLightbox.js
@@ -13,15 +13,30 @@ export function openLightBox(e) {
             main.slideToLoop(index, 0);
         }, 50);
     });
+
+    document.documentElement.classList.add('lightbox-open');    
 };
 
 const closeButton = document.getElementById("close-modal");
 
 closeButton.addEventListener("click", () => {
+    const activeImg = document.querySelector(
+        '#lightbox .swiper-container[data-scope="modal"] .mySwiper2 .swiper-slide-active img'
+    );
+
+    const index = Number(activeImg?.dataset.index ?? 0);
+    
     document.getElementById("lightbox").classList.add("hidden");
 
-    // getProductData(id).then(product => {
-    //     initializeSwipers("gallery", product);
-    //     setProductDetails(product);
-    // });
+    const id = Number(document.getElementById("product").dataset.id);
+
+    getProductData(id).then(product => {
+        const { main } = initializeSwipers("gallery", product);
+
+        setTimeout(() => {
+            main.slideToLoop(index, 0);
+        }, 50);
+    });
+
+    document.documentElement.classList.remove('lightbox-open');
 });
