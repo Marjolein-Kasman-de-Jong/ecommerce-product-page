c1182add29b11c9cc3e916425d668e3a76a68cb8
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2025-10-24 14:19:11 +0200

    Enhance code readability

--- assets/scripts/modules/setProductDetails.js
+++ assets/scripts/modules/setProductDetails.js
@@ -1,12 +1,22 @@
-const [brand, productName, description, finalPrice, discount, originalPrice, productID] =
-    ["brand", "name", "description", "final-price", "discount", "original-price", "product-id"]
+export function setProductDetails(product) {
+    const [brand, productName, description, finalPrice, discount, originalPrice, productID] =
+        [
+            "brand", 
+            "name", 
+            "description", 
+            "final-price", 
+            "discount", 
+            "original-price", 
+            "product-id"
+        ]
         .map(id => document.getElementById(id));
 
-export function setProductDetails(product) {
+    const discountedPrice = product.price * (1 - product.discount / 100);
+
     brand.textContent = product.brand;
     productName.textContent = product.name;
     description.textContent = product.description;
-    finalPrice.textContent = `$${(product.price * (1 - product.discount / 100)).toFixed(2)}`;
+    finalPrice.textContent = `$${discountedPrice.toFixed(2)}`;
     discount.textContent = `${product.discount}%`;
     originalPrice.textContent = `$${product.price.toFixed(2)}`;
     productID.value = product.name;
